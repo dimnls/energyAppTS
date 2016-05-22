09d42f835580b78482cd5114831094baccd5687f
Dimitrios Man <Dimitrios Man>
2016-05-22 23:40:20 +0300

    Working sidemenu, only UserInfo

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,46 @@
-import {App, Platform, Storage, LocalStorage} from 'ionic-angular';
+import {App, Platform, Storage, LocalStorage, MenuController, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
+import {ViewChild} from '@angular/core';
+
 import {TabsPage} from './pages/tabs/tabs';
 import {IntroPage} from './pages/intro/intro';
 import {DataService} from './providers/data/data';
+import {UserInfoPage} from './pages/user-info/user-info'; //REMEMBER to import all pages needed for the menu
 
 @App({
   // template: '<ion-nav [root]="rootPage"></ion-nav>',
   templateUrl: 'build/app.html', //FOR MENU
   providers: [DataService],
-  config: { tabbarPlacement: 'bottom' } // http://ionicframework.com/docs/v2/api/config/Config/
+  config: {
+    tabbarPlacement: 'bottom'
+  } // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
   rootPage: any = TabsPage;
+  menuPages: Array<{title: string, component: any}>;
   local: LocalStorage;
 
 
-  constructor(platform: Platform) {
+  constructor(private platform: Platform, private menu: MenuController) {
+    this.initializeApp();
+    this.menuPages = [
+      {title: 'User Info', component: UserInfoPage}
+    ];
+
+    this.goToIntroOrHome();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+  //Check if it's first run and show Intro if it is. Other wise, jump into app
+  goToIntroOrHome() {
     this.local = new Storage(LocalStorage);
     this.local.get('introShown').then((result: any) => {
       if(result) {
@@ -26,13 +51,13 @@ export class MyApp {
         console.log('introShown='+this.local.get('introShown'));
       }
     })
-
-
-
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
   }
+
+  openMenuPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.push(page.component);
+  }
+
 }
